f01dc0deeffa6c24c3a6f4c2af3389ee723f69e3
Isaac Romarri <iromero385@berkeley.edu>
2018-08-14 09:13:38 -0700

    fixed bug that was not targeting new buttons

--- assets/Javascript/Java.js
+++ assets/Javascript/Java.js
@@ -1,17 +1,18 @@
-  var topics =["Friends", "Fresh Prince", "Charmed", "Buffy The Vampire Slayer"]
-  var initializingButtons = function(){
-    $("#buttonArea").empty();
+var topics =["Friends", "Fresh Prince", "Charmed", "Buffy The Vampire Slayer"]
+var initializingButtons = function(){
+  $("#buttonArea").empty();
     for(key in topics){
+      debugger;
       var newBotton = $("<button>")
       newBotton.attr("class","btn btn-success p-1 m-1 gifButton")
       newBotton.attr("data-tag",topics[key]);
       newBotton.text(topics[key]);
+      
       $("#buttonArea").append(newBotton);
     }
-  }
-  initializingButtons();
-  $(".gifButton").on("click", function(event) {
-    debugger;
+  }   
+initializingButtons();
+  $(document).on("click",".gifButton", function() {
       var newTag = $(this).attr("data-tag")
       // Storing our giphy API URL for a random cat image
       var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + newTag + "&api_key=1XnieqPzGYUSuP5YhR6gY2Suzx8kh4iw&limit=10";
@@ -19,12 +20,7 @@
       $.ajax({
         url: queryURL,
         method: "GET"
-      })
-  
-      // After the data from the AJAX request comes back
-        .then(function(response) {
-          console.log(response);
-        
+      }).then(function(response) {     
           for(var i = 0; i < 10 ; i++){
             var stillUrl = response.data[i].images.fixed_height_still.url;
             var moveUrl = response.data[i].images.fixed_height.url;
@@ -41,13 +37,13 @@
            
   
             // Prepending the catImage to the images div
-            $("#images").prepend(rated);
             $("#images").prepend(newImage);
+            $("#images").prepend("<h3 text-info>"+"Rating: "+rated+"</h3>");
           }
         });
-    });
+  });
   
-    $(document).on('click', '.clickBait', function(){
+  $(document).on('click', '.clickBait', function(){
       var state = $(this).attr('data-state');
       var moving = $(this).attr('data-animate');
       var still = $(this).attr('data-still');
@@ -60,25 +56,12 @@
         $(this).attr('src', still);
         $(this).attr('data-state', 'still');
       }
-    })
+  });
   
   $("#addTag").on("click", function(event){
     event.preventDefault();
     var newTopic = $("#newTag").val().trim();
-    $("#newTag").val("")
     topics.push(newTopic);
+    $("#newTag").val("")
     initializingButtons();
-
-    // var newBotton = $("<button>")
-    // newBotton.attr("class","btn btn-success p-1 m-1 gifButton")
-    // var textLabel = $("#newTag").val();
-    // newBotton.text(textLabel);
-    // $("#buttonArea").append(newBotton);
-    // $("#newTag").val("");
-  
-  
-  
-  })
-  
-
-
+  });
